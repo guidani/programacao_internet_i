087e91c6c1ad1261f6aa4f7648986121ab709758
guilherme-win <gui.dani13@gmail.com>
2021-03-18 15:29:34 -0300

    hello world em reactjs v1

---
+++ hello_world/reactjs/app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* O código vai aqui */}
+      <p>Hello World</p>
+    </div>
+  );
+}
+
+export default App;
