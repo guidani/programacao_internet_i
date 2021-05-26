583e78c02484d620ad4d8684ab4f571944cd2d56
guilherme daniel <gui.dani13@gmail.com>
2021-05-26 18:48:48 -0300

    Reup dos arquivos
    O blog anterior apresentou problemas então foi criado um novo blog

--- django_girls_tutorial/mysite/blog/models.py
+++ django_girls_tutorial/mysite/blog/models.py
@@ -1,10 +1,11 @@
 from django.conf import settings
 from django.db import models
+from django.db.models.deletion import CASCADE
 from django.utils import timezone
 
-# Create your models here.
+
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
@@ -14,5 +15,6 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
+    
     def __str__(self):
         return self.title
