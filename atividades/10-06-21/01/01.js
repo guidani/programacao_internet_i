badfb63fc5de1461ff555f858551de494948975f
Guilherme Daniel <gui.dani13@gmail.com>
2021-06-15 21:33:18 -0300

    adicionado atividade typescript 2

---
+++ atividades/10-06-21/01/01.js
@@ -0,0 +1,55 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var DefineColecao;
+(function (DefineColecao) {
+    var CriaMetodos = /** @class */ (function () {
+        function CriaMetodos(_a) {
+            this.a = [];
+        }
+        CriaMetodos.prototype.proximo = function (i) {
+            return this.a[i + 1];
+        };
+        CriaMetodos.prototype.atual = function (i) {
+            return this.a[i];
+        };
+        CriaMetodos.prototype.isPrimeiro = function (i) {
+            return i == 0;
+        };
+        CriaMetodos.prototype.isUltimo = function (i) {
+            return i != 0;
+        };
+        CriaMetodos.prototype.irParaPrimeiro = function (i) {
+            return this.a[0];
+        };
+        return CriaMetodos;
+    }());
+    var ColecaoArray = /** @class */ (function (_super) {
+        __extends(ColecaoArray, _super);
+        function ColecaoArray() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        ColecaoArray.prototype.adiciona = function (values) {
+            this.a.push(values);
+        };
+        return ColecaoArray;
+    }(CriaMetodos));
+    var colecao = new ColecaoArray([]);
+    colecao.adiciona(1);
+    colecao.adiciona(2);
+    colecao.adiciona(3);
+    colecao.adiciona(4);
+    colecao.adiciona(5);
+})(DefineColecao || (DefineColecao = {}));
---
+++ atividades/10-06-21/01/01.ts
@@ -0,0 +1,54 @@
+namespace DefineColecao {
+    interface Iteravel<Tipo> {
+
+        a: Array<Tipo>;
+        proximo(i: number): Tipo;
+        atual(i: number): Tipo;
+        isPrimeiro(i: number): boolean;
+        isUltimo(i: number): boolean;
+
+    }
+
+    class CriaMetodos<Tipo> implements Iteravel<Tipo> {
+        a: Array<Tipo>;
+
+        constructor([]: Iterable<Tipo>) {
+            this.a = [];
+        }
+
+        proximo(i: number): Tipo { 
+            return this.a[i + 1];
+        }
+
+        atual(i: number): Tipo {
+            return this.a[i];
+        }
+
+        isPrimeiro(i: number): boolean {
+            return i == 0;
+        }
+
+        isUltimo(i: number): boolean {
+            return i != 0;
+        }
+
+        irParaPrimeiro(i: Iteravel): Iteravel{
+            return this.a[0];
+        }
+    }
+
+    class ColecaoArray<Tipo> extends CriaMetodos<Tipo> {
+        adiciona(values: Tipo) {
+            this.a.push(values);
+        }
+    }
+
+    let colecao = new ColecaoArray<number>([]);
+
+    colecao.adiciona(1);
+    colecao.adiciona(2);
+    colecao.adiciona(3);
+    colecao.adiciona(4);
+    colecao.adiciona(5);
+}
+  
